34eab72ded92c1afcd03370982c7298489dbe3e6
kenodek <dominik.targosz@protonmail.com>
2020-08-30 11:08:06 +0200

    Updates

---
+++ precache-manifest.00acded5c68c20be08e7b0b7a80c1ede.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e07337c0acc9532a5f6e5cf906437df",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "6ecdd55e1bce2dd05b08",
+    "url": "/chat/static/css/2.1bce3852.chunk.css"
+  },
+  {
+    "revision": "87a3599ea1b1720bbb6d",
+    "url": "/chat/static/css/main.382f24cb.chunk.css"
+  },
+  {
+    "revision": "6ecdd55e1bce2dd05b08",
+    "url": "/chat/static/js/2.a7483f08.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/chat/static/js/2.a7483f08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87a3599ea1b1720bbb6d",
+    "url": "/chat/static/js/main.a39aba37.chunk.js"
+  },
+  {
+    "revision": "f81e32e008a30fec5fcf",
+    "url": "/chat/static/js/runtime-main.7299832f.js"
+  },
+  {
+    "revision": "a2078890353e2040be3cff332db97400",
+    "url": "/chat/static/media/transparent_bg.a2078890.jpg"
+  }
+]);
